53c1fd3030e642af3a96f3fb78e5455d11cfc699
LukaMelinc <luka.melinc1@gmail.com>
2024-03-31 12:38:22 +0200

    updated

--- main2.py
+++ main2.py
@@ -10,19 +10,40 @@ def dec_to_bin(vrednost):
         vrednost = vrednost // 2  # Celodelno deljenje z 2
     return bin_vrednost
 
+def dec_to_hex(vrednost):
+    if vrednost == 0:
+        return "0"
+    hex_znaki = "0123456789ABCDEF"  # nabor hex znakov za filanje hex vrednosti
+    hex_vrednost = ""   # prazna postavljena hex vrednost
+    while vrednost > 0:
+        ostanek = vrednost % 16 # ostanek pri deljenju z 16 nam pove naslednji znak v hex vrednosti
+        hex_vrednost = hex_znaki[ostanek] + hex_vrednost       # dodajanje znaka v hex vredndost
+        vrednost = vrednost // 16   # 
+    return hex_vrednost
+
+
+def pretvori_v_bajte_in_znak(stevilka):
+    bajti = bytes([int(i, 2) for i in bit_size(stevilka)])
+    znak = bajti.decode('utf-8').replace("\n", "\\n").replace("\r", "\\r")
+    return bajti, znak
+
+
 #print(dec_to_bin(269))
+# UTF-8 strukturea predpon (glej tabelo v navodilih)
 def bit_size(vrednost): #calculate the number of bits necesearry to code the number to binary
-    min_stevilo_bitov = np.log2(vrednost)
+
+    min_stevilo_bitov = np.log2(vrednost)       # minimalno število bitov, da se lahko zapiše vrednost
+    
     if min_stevilo_bitov <= 7:
-        return [bin(vrednost)[2:]]
+        return [dec_to_bin(vrednost)[2:]]
     elif min_stevilo_bitov <= 11:
-        binarna = bin(vrednost)[2:].zfill(11)
+        binarna = dec_to_bin(vrednost)[2:].zfill(11)
         return ["110"+binarna[:5], "10"+binarna[5:]]
     elif min_stevilo_bitov <= 16:
-        binarna = bin(vrednost)[2:].zfill(16)
+        binarna = dec_to_bin(vrednost)[2:].zfill(16)
         return ["1110"+binarna[:4], "10"+binarna[4:10], "10"+binarna[10:]]
     elif min_stevilo_bitov <= 21:
-        binarna = bin(vrednost)[2:].zfill(21)
+        binarna = dec_to_bin(vrednost)[2:].zfill(21)
         return ["11110"+binarna[:3], "10"+binarna[3:9], "10"+binarna[9:15], "10"+binarna[15:]]
     else:
         print("Stevilka je prevelika")
@@ -33,21 +54,23 @@ if __name__ == "__main__":
     tabela_stevil = [int(i) for i in besedilo.replace(' ', '').split(',')] # odstramnimo vse presledke, zgradimo slovar znakovnih nizov iz besedila
                                                                            # s split razdelimo besede na znakovne nize
     
-    with open("besedilo.txt", "wb") as f:   # postavimo novo datoteko besedilo.txt v repozitoriju, če taka datoteka že obstaja, jo povozimo: wb -> write binary
+    with open("besedilo.txt", "wb") as besedilo:   # postavimo novo datoteko besedilo.txt v repozitoriju, če taka datoteka že obstaja, jo povozimo: wb -> write binary
         for stevilka in tabela_stevil: # iterira skozi vse znakovne nize iz vhodnega besedila, ki smo jih razdelili z vejicami
             # tabelo binarnih kod pretvorimo v tabelo števil in nato v tabelo bajtov (bytes), ki jih zapišemo v datoteko
-            f.write(bytes([int(i, 2) for i in bit_size(stevilka)]))
+            besedilo.write(bytes([int(i, 2) for i in bit_size(stevilka)]))
 
     unikatni_znaki = list(set(tabela_stevil))  # set() je množica, ki ne vsebuje ponovitev
     unikatni_znaki.sort()
-    with open("tabela_kodiranja.txt", "w", encoding="utf-8") as f:
-        for stevilka in unikatni_znaki:
-            binarna = "".join(bit_size(stevilka))  # pretvorimo tabelo binarnih kod v eno veliko binarno število npr. ['11001000', '10100000'] -> '1100100010100000'
-            decimalna = int(binarna, 2)  # pretvorimo binarno število v desetiško število
-            sestnajstiska = hex(decimalna)[2:].upper().zfill(4)  # pretvorimo desetiško število v šestnajstiško število
-            znak = (bytes([int(i, 2) for i in bit_size(stevilka)]).decode('utf-8')).replace("\n", "\\n").replace("\r", "\\r")
-            print(f"Unicode št. {stevilka} = {znak}, dec={decimalna}, hex={sestnajstiska}, bin={binarna}")
-            f.write(f"{stevilka};{znak};{binarna};{decimalna};{sestnajstiska}\n")
+
+with open("tabela_kodiranja.txt", "w", encoding="utf-8") as datoteka:
+    for stevilka in unikatni_znaki:
+        binarna = "".join(bit_size(stevilka))
+        decimalna = int(binarna, 2)
+        sestnajstiska = hex(decimalna)[2:].upper().zfill(4)
+        _, znak = pretvori_v_bajte_in_znak(stevilka)  # '_' ignorira bajte, ker jih tukaj ne potrebujemo
+
+        print(f"Unicode št. {stevilka} = {znak}, dec={decimalna}, hex={sestnajstiska}")
+        datoteka.write(f"{stevilka};{znak};{binarna};{decimalna};{sestnajstiska}\n")   
 
 
 
